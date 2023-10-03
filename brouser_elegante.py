db2bbc554c257b5b38155d97fa8b59cee9f5eb68
fabianarsff <fabianarsantos@gmail.com>
2023-10-02 22:48:56 -0300

    contador de palavras/teste

--- brouser_elegante.py
+++ brouser_elegante.py
@@ -2,5 +2,13 @@ import urllib.request, urllib.parse, urllib.error
 
 
 fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+"""for line in fhand:
+    print(line.decode().strip())"""
+
+
+counts: dict[str, int] = {}
 for line in fhand:
-    print(line.decode().strip())
+    words = line.decode().split()
+    for word in words:
+        counts[word] = counts.get(word, 0) + 1
+print(counts)
